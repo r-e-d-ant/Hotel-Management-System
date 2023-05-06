f0e231d9777a75263b9239504cc6f5ab080eac67
red_ant <mugishathierry65@gmail.com>
2023-05-06 18:23:22 +0200

    added the line to call the function of creating the database and tables

--- src/view/Login.java
+++ src/view/Login.java
@@ -25,6 +25,7 @@ public class Login extends javax.swing.JFrame {
      */
     public Login() {
         initComponents();
+	createDBandTables();
     }
     
     // check if theres database and tables
